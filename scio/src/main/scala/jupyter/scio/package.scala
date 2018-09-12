af323e90244c65dad83ab1f915283c82e3000be4
Anish <anish749@gmail.com>
2018-09-12 22:38:25 +0200

    add show

--- scio/src/main/scala/jupyter/scio/package.scala
+++ scio/src/main/scala/jupyter/scio/package.scala
@@ -1,11 +1,13 @@
 package jupyter
 
-import _root_.scala.tools.nsc.interpreter.Helper
-
 import java.io.File
 
 import com.google.auth.Credentials
+import com.spotify.scio.ScioContext
 import com.spotify.scio.bigquery.BigQueryClient
+import com.spotify.scio.values.SCollection
+
+import _root_.scala.tools.nsc.interpreter.Helper
 
 package object scio {
 
@@ -20,5 +22,32 @@ package object scio {
 
   def bigQueryClient(project: String, secretFile: File): BigQueryClient =
     Helper.bigQueryClient(project, secretFile)
+
+  implicit class JupyterSCollection[T](self: SCollection[T]) {
+
+    /**
+     * Get first n elements of the SCollection as a String separated by \n
+     */
+    private def asString(numElements: Int): String = {
+      val sc = self.context
+
+      val materializedSelf = self
+        .withName(s"Take $numElements")
+        .take(numElements)
+        .materialize
+
+      sc.close().waitUntilDone()
+      materializedSelf
+        .waitForResult() // Should be ready
+        .value
+        .mkString("\n")
+    }
+
+    /**
+     * Print elements on screen
+     */
+    def show(numElements: Int = 20): Unit = println(asString(numElements))
+
+  }
 
 }
