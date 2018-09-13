14f1df8b43897619281d7c8bfc5e4426f9b6daf2
Anish <anish749@gmail.com>
2018-09-13 18:06:04 +0200

    enhanced scio context for jupyter

--- scio/src/main/scala/com/spotify/scio/jupyter/JupyterScioContext.scala
+++ scio/src/main/scala/com/spotify/scio/jupyter/JupyterScioContext.scala
@@ -20,57 +20,58 @@ import scala.collection.JavaConverters._
 
 // in the com.spotify.scio namespace to access private[scio] things
 
-class JupyterScioContext(
-  options: PipelineOptions,
-  replJarPath: Path
-)(implicit
-  interpApi: InterpAPI,
-  runtimeApi: RuntimeAPI
-) extends ScioContext(options,
+class JupyterScioContext(options: PipelineOptions,
+                         replJarPath: Path)
+                        (implicit interpApi: InterpAPI,
+                         runtimeApi: RuntimeAPI
+                        ) extends ScioContext(options,
   replJarPath.toAbsolutePath.toString ::
     runtimeApi.sess.frames
       .flatMap(_.classpath)
       .map(_.getAbsolutePath)
 ) {
 
-  private val logger = LoggerFactory.getLogger(this.getClass)
+  import JupyterScioContext._
 
   interpApi.load.onJarAdded {
     case Seq() => // just in case
-    case jars =>
+    case _ =>
       throw new RuntimeException("Cannot add jars after ScioContext Initialization")
   }
 
-  def setGcpCredentials(credential: Credentials): Unit =
-    options.as(classOf[DataflowPipelineOptions]).setGcpCredential(credential)
+  def setGcpCredential(credentials: Credentials): Unit = {
+    // Save credentials for all future context creation
+    _gcpCredentials = Some(credentials)
 
-  def setGcpCredentials(path: String): Unit =
-    setGcpCredentials(
+    require(_currentContext.isDefined && !_currentContext.get.isClosed,
+      "Scio Context is not yet defined or already closed")
+
+    _currentContext.get.options.as(classOf[DataflowPipelineOptions])
+      .setGcpCredential(credentials)
+  }
+
+  def setGcpCredential(path: String): Unit =
+    setGcpCredential(
       GoogleCredentials.fromStream(new FileInputStream(new File(path))).createScoped(
         List(DataflowScopes.CLOUD_PLATFORM).asJava
       )
     )
 
-  def withDefaultGcpCredentials(): this.type = {
-    setGcpCredentials(GoogleCredentials.getApplicationDefault.createScoped(
-      List(DataflowScopes.CLOUD_PLATFORM).asJava)
-    )
-    this
+  def withGcpCredential(credentials: Credentials): JupyterScioContext = {
+    setGcpCredential(credentials)
+    _currentContext.get
   }
 
-  def withGcpCredentials(credentials: Credentials): this.type = {
-    setGcpCredentials(credentials)
-    this
-  }
-
-  def withGcpCredentials(path: String): this.type = {
-    setGcpCredentials(path)
-    this
+  def withGcpCredential(path: String): JupyterScioContext = {
+    setGcpCredential(path)
+    _currentContext.get
   }
 
   /** Enhanced version that dumps REPL session jar. */
   override def close(): ScioResult = {
-    logger.info("Closing Scio Context") // Some APIs exposed only for Jupyter might close
+    // Some APIs exposed only for Jupyter will close Scio Context
+    // even if the user intends to use it further. A new Context would be created again.
+    logger.info("Closing Scio Context")
     runtimeApi.sess.sessionJarFile(replJarPath.toFile)
     super.close()
   }
@@ -82,37 +83,56 @@ class JupyterScioContext(
 
 }
 
-private[jupyter] object JupyterScioContext {
+/**
+ * Allow only one active Scio Context.
+ * Also manage the existing Scio Context, create a new one if the current has been closed.
+ */
+object JupyterScioContext {
 
-  def apply(args: (String, String)*)(implicit
-    interpApi: InterpAPI,
-    runtimeApi: RuntimeAPI
-  ): JupyterScioContext =
-    JupyterScioContext(
-      PipelineOptionsFactory.fromArgs(
-        args
-          .map { case (k, v) => s"--$k=$v" }: _*
-      ).as(classOf[DataflowPipelineOptions]),
-      nextReplJarPath()
-    )
+  private val logger = LoggerFactory.getLogger(this.getClass)
 
-  def apply(options: PipelineOptions)(implicit
-    interpApi: InterpAPI,
-    runtimeApi: RuntimeAPI
-  ): JupyterScioContext =
-    JupyterScioContext(options, nextReplJarPath())
-
-  def apply(
-    options: PipelineOptions,
-    replJarPath: Path
-  )(implicit
-    interpApi: InterpAPI,
-    runtimeApi: RuntimeAPI
-  ): JupyterScioContext =
-    new JupyterScioContext(options, replJarPath)
+  private var _currentContext: Option[JupyterScioContext] = None
+  private var _pipelineOptions: Option[PipelineOptions] = None
+  private var _gcpCredentials: Option[Credentials] = None
 
 
-  def nextReplJarPath(prefix: String = "jupyter-scala-scio-", suffix: String = ".jar"): Path =
+  /**
+   * Always returns a new Scio Context, and forgets the old context
+   */
+  def apply(args: (String, String)*)
+           (implicit interpApi: InterpAPI,
+            runtimeApi: RuntimeAPI): Unit = JupyterScioContext(
+    PipelineOptionsFactory.fromArgs(
+      args.map { case (k, v) => s"--$k=$v" }: _*
+    ).as(classOf[PipelineOptions])
+  )
+
+  /**
+   * Always returns a new Scio Context, and forgets the old context
+   */
+  def apply(options: PipelineOptions)
+           (implicit interpApi: InterpAPI, runtimeApi: RuntimeAPI): Unit = {
+    _pipelineOptions = Some(options)
+    _currentContext = Some(new JupyterScioContext(options, nextReplJarPath()))
+    logger.info("ScioContext is accessible as sc")
+  }
+
+  /**
+   * Get Scio Context with currently defined options.
+   * Get new Scio Context if previous is closed.
+   *
+   * @return
+   */
+  def sc(implicit interpApi: InterpAPI, runtimeApi: RuntimeAPI): JupyterScioContext = {
+    if (_currentContext.isEmpty || _currentContext.get.isClosed) {
+      // Create a new Scio Context
+      JupyterScioContext(_pipelineOptions.getOrElse(PipelineOptionsFactory.create()))
+      _gcpCredentials.foreach(_currentContext.get.setGcpCredential)
+    }
+    _currentContext.get
+  }
+
+  private def nextReplJarPath(prefix: String = "jupyter-scala-scio-", suffix: String = ".jar"): Path =
     Files.createTempFile(prefix, suffix)
 
 }
